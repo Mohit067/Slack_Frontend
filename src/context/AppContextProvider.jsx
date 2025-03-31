6750e622fad930ea7d2867f4b39f543d3bafa79a
Mohit sahu <mohitsahu60067@gmail.com>
2025-03-31 13:15:53 +0530

    socke client added

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -4,8 +4,10 @@ import { CreateWorkspaceContextProvider } from "./CreateWorkspaceContext";
 import { WorkspacePrefrenceModalContextProvider } from "./WorkspacePrefrenceModalContext";
 import { CreateChannelContextProvider } from "./CreateChannelContext";
 import { WorkspaceContextProvider } from "./WorkspaceContext";
+import { SocketContextProvider } from "./SocketContext";
 
 export const AppContextProvider = conmbineContext(
+    SocketContextProvider,
     AuthContextProvider,
     CreateWorkspaceContextProvider,
     WorkspacePrefrenceModalContextProvider,
